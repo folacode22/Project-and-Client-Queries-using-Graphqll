a3253dcba277311573565547bb44ac1b1bf0a63a
Agbabiaka Omofolarin <folarinagbabiaka2024@gmail.com>
2024-06-10 21:03:35 +0100

    "update client"

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Clients from '../components/Clients';
+import Projects from '../components/Projects';
+import AddClientModal from '../components/AddClientModel';
+import AddProjectModal from '../components/AddProjectModel';
+
+export default function Home() {
+  return (
+    <>
+      <div className='d-flex gap-3 mb-4'>
+        <AddClientModal />
+        <AddProjectModal />
+      </div>
+      <Projects />
+      <hr />
+      <Clients />
+    </>
+  );
+}
